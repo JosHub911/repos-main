f50c41a565b75076fbcc27f4552f5ca4db0fda50
JosHub911 <166252789+JosHub911@users.noreply.github.com>
2024-09-25 17:03:36 +0200

    Update Playerdata.js

--- Playerdata.js
+++ Playerdata.js
@@ -1,12 +1,12 @@
 let name = "albert appels"
 console.log("albert:" + appels);
-let Height = "6 ft"
+let Height = 6
 console.log("Height:" + Height);
 let Power = 560
 console.log("Power" + Power);
 Let HP = 6000
 console.log("HP:" + HP);
-let Cursed = "True";
+let Cursed = True;
 console.log("Cursed:" + Cursed);
 let Lives = 1
 console.log("Lives:" + Lives);
@@ -14,5 +14,5 @@ let Lasers = 5
 console.log("Lasers:" + Lasers);
 let Battery = 90
 console.log("Battery:" + Battery);
-let Wanted = "True";
+let Wanted = True;
 connsole.log("Wanter:" + Wanted);
