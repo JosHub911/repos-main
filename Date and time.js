8dba5fdd4f6555d92ba5e96efce70ea6c95793ac
JosHub911 <166252789+JosHub911@users.noreply.github.com>
2024-09-25 17:20:20 +0200

    Update Date and time.js

--- Date and time.js
+++ Date and time.js
@@ -1,11 +1,11 @@
-const date = new Date(1985, 7, 12, 39, 5, 10)
+const date = new Date(1985, 7, 12, 39, 5, 10);
 console.log(date);
 
-const date2 = new Date(1955, 9, 23, 39, 5, 10)
+const date2 = new Date(1955, 9, 23, 39, 5, 10);
 console.log(date2);
 
-const date3 = new Date(2015, 8, 31, 39, 5, 10)
+const date3 = new Date(2015, 8, 31, 39, 5, 10);
 console.log(date3);
 
-const date4 = new Date(1885, 1, 4, 39, 5, 10)
+const date4 = new Date(1885, 1, 4, 39, 5, 10);
 console.log(date4);
