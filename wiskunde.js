c2c7a00f077f2e4fa19eda1d26fb7c7dc57253ff
JosHub911 <166252789+JosHub911@users.noreply.github.com>
2024-10-02 16:38:52 +0200

    Update wiskunde.js

--- wiskunde.js
+++ wiskunde.js
@@ -5,23 +5,25 @@ return lengte * breedte * hoogte
 let kubus = kubusinhoud(10, 5, 10);
 console.log(kubus);
 
-function cilinderInhoud(diameter, hoogte2){
-    let pi = Math.PI
-    return diameter * pi * hoogte2
+function cilindir(diameter, hoogte1){
+    let p2i = Math.PI
+    let radius = diameter / 2 ;
+    return Math.pow(radius,2) * p2i * hoogte1
+}
 
+let inhoudK = cilindir(20, 50);
+console.log(inhoudK); 
+function driehoek(lengte2, hoogte2){
+    return Math.sqrt(Math.pow(lengte2, 2) + Math.pow(hoogte2, 2));
+}
+
+let drie = driehoek(20,50);
+console.log(drie)
+
+    function getallen(one,two,three,four,five,six,seven){
+        let middle = one + two + three + four + five + six + seven
+        return middle / 7
     }
     
-    let cilinder = cilinderInhoud(10, 5);
-    console.log(cilinder);
-
-    function driekoek(korte, korte2){
-        let Lange = Math.pow(korte,2)
-        return Math.pow(korte2,2) + Lange
-
-    }
-
-    let drh = driekoek(10,30);
-    console.log(drh)
-
-
-    
+    let middle = getallen(2,6,4,3,7,2,5)
+    console.log(middle)
